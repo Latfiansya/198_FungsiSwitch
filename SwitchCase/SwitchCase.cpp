4b66d0c8cbe68a8c31f23a6ff7259bc8d4a198b2
Latfiansya <latfiansyadzikri@gmail.com>
2024-03-26 10:50:51 +0700

    Looping dengan do...while

--- SwitchCase/SwitchCase.cpp
+++ SwitchCase/SwitchCase.cpp
@@ -30,3 +30,50 @@ void inputdata()
 	cout << "Masukkan bilangan kedua : ";
 	cin >> bilangan2;
 }
+
+int main()
+{
+	int pilihan;
+	do
+	{
+		system("CLS");
+		cout << "Menu Kalkulator Sederhana\n";
+		cout << "1. Penjumlahan\n";
+		cout << "2. Pengurangan\n";
+		cout << "3. Perkalian\n";
+		cout << "4. Pembagian\n";
+		cout << "5. Keluar\n";
+		cout << "Masukkan pilhan : ";
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
+		case 1 :
+			inputdata();
+			cout << "Hasil pernjumlahan : " << penjumlahan(bilangan1, bilangan2) << endl;
+			system("pause");
+			break;
+		case 2 :
+			inputdata();
+			cout << "Hasil pengurangan : " << pegurangan(bilangan1, bilangan2) << endl;
+			system("pause");
+			break;
+		case 3:
+			inputdata();
+			cout << "Hasil perkalian : " << perkalian(bilangan1, bilangan2) << endl;
+			system("pause");
+			break;
+		case 4:
+			inputdata();
+			cout << "Hasil pembagian : " << pembagian(bilangan1, bilangan2) << endl;
+			system("pause");
+			break;
+		case 5 :
+			break;
+		default :
+			cout << "Pilihan tidak teridentifikasi\n";
+			system("pause");
+			break;
+		}
+	} while (pilihan != 5);
+}
